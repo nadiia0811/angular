7f8a8f4995dc804729258bbf4898c078cacae089
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-04 13:55:51 +0000

    feat(app): introduce App root component

--- tik-talk/src/app/app.ts
+++ tik-talk/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ProfileCard } from './common-ui/profile-card/profile-card';
+import { ProfileService } from './data/services/profile';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProfileCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('tik-talk');
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService
+      .getTestAccounts()
+      .subscribe(val => this.profiles = val);
+  }
 }
