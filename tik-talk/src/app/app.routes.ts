70921b936d092fc995723fdf26ca69576b79577a
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-05 12:47:41 +0000

    chore(route): implement routes for LoginPage and Layout components

--- tik-talk/src/app/app.routes.ts
+++ tik-talk/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login';
+import { SearchPage } from './pages/search-page/search-page';
+import { ProfilePage } from './pages/profile-page/profile-page';
+import { Layout } from './common-ui/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: Layout, children: [
+      { path: 'profile', component: ProfilePage },
+      { path: '', component: SearchPage },
+    ]
+  },
+  { path: 'login', component: LoginPage },
+];
