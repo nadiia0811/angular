84683e3bb36d7ba9f675e5c494ea2584b49f6fb6
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-06 09:55:31 +0000

    feat(auth): implement AuthService with guard and integrate into routes

--- tik-talk/src/app/app.routes.ts
+++ tik-talk/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { LoginPage } from './pages/login/login';
 import { SearchPage } from './pages/search-page/search-page';
 import { ProfilePage } from './pages/profile-page/profile-page';
 import { Layout } from './common-ui/layout/layout';
+import { canActivateAuth } from './auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: '', component: Layout, children: [
       { path: 'profile', component: ProfilePage },
       { path: '', component: SearchPage },
-    ]
+    ],
+    canActivate: [canActivateAuth]
   },
   { path: 'login', component: LoginPage },
 ];
---
+++ tik-talk/src/app/auth/auth.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { TokenResponse } from './auth.interface';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  http = inject(HttpClient);
+  baseUrl = 'https://icherniakov.ru/yt-course/auth';
+  token: string | null = null;
+  refreshToken: string | null = null;
+  cookieService = inject(CookieService);
+
+  get isAuth() {
+    if(!this.token) {
+      this.token = this.cookieService.get('token');
+    }
+    return !!this.token;
+  }
+
+  login(payload: { username: string, password: string }) {
+    const formData = new FormData();
+    formData.append('username', payload.username);
+    formData.append('password', payload.password);
+
+    return this.http
+      .post<TokenResponse>(`${this.baseUrl}/token`, formData)
+      .pipe(
+        tap(val => {
+          this.token = val.access_token;
+          this.refreshToken = val.refresh_token;
+
+          this.cookieService.set('token', this.token);
+          this.cookieService.set('refreshToken', this.refreshToken);
+        })
+      );
+  }
+}
