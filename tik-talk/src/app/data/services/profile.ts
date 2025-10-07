d73bc90cf9f60d8722678e5a35d6329a69c9bc95
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-07 09:35:09 +0000

    feat(profile): add getMe method to fetch current user data

--- tik-talk/src/app/data/services/profile.ts
+++ tik-talk/src/app/data/services/profile.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Profile } from '../interfaces/profile.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
   http = inject(HttpClient);
-  baseUrl = 'https://icherniakov.ru/yt-course/';
+  baseUrl = 'https://icherniakov.ru/yt-course';
 
   getTestAccounts() {
-    return this.http.get(`${this.baseUrl}account/test_accounts`);
+    return this.http.get<Profile[]>(`${this.baseUrl}/account/test_accounts`);
+  }
+
+  getMe() {
+    return this.http.get<Profile>(`${this.baseUrl}/account/me`);
   }
 }
