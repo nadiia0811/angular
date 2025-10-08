2a5557484ae9a30c50e0635a34899c9a69a41dc3
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-08 14:45:43 +0000

    feat(svg-icon): implement SvgIcon component to display svgs from icons.svg by id

---
+++ tik-talk/src/app/common-ui/svg-icon/svg-icon.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'svg[icon]',
+  imports: [],
+  template: `
+    <svg width="16" height="16" viewBox="0 0 16 16">
+      <use [attr.href]="href"></use>
+    </svg>
+  `,
+  styles: ['']
+})
+export class SvgIcon {
+  @Input() icon: string = ''
+
+  get href() {
+    return `/assets/images/icons.svg#${this.icon}`;
+  }
+}
