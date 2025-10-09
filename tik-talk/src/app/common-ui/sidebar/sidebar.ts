5f865e70c22349450c2b9f6fe156ea15175eb694
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-09 15:38:53 +0000

    feat(sidebar): implement reusable Sidebar component

---
+++ tik-talk/src/app/common-ui/sidebar/sidebar.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { SvgIcon } from '../svg-icon/svg-icon';
+import { SubscriberCard } from './subscriber-card/subscriber-card';
+import { RouterModule } from '@angular/router';
+import { ProfileService } from '../../data/services/profile';
+import { AsyncPipe } from '@angular/common';
+import { firstValueFrom, map } from 'rxjs';
+import { ImgPipe } from '../../helpers/pipes/img-pipe';
+
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [SvgIcon, SubscriberCard, RouterModule, AsyncPipe, ImgPipe],  
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss'
+})
+export class Sidebar {
+  menuItems = [
+    {icon: "home", label: "My page", link: ""},
+    {icon: "search", label: "Search", link: "search"},
+    {icon: "chat", label: "Chats", link: "chats"},
+  ]
+
+  profileService = inject(ProfileService);
+  me = this.profileService.me;
+
+  subscribers$ = this.profileService
+    .getSubscribersShortlist()
+    .pipe(
+      map(res => res.items.slice(0, 3))
+    ); 
+
+  ngOnInit() {
+    firstValueFrom(this.profileService.getMe());
+  }
+}
