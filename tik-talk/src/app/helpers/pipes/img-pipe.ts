027e7fb22b75e5dcec114d0aef41d4dd04a90310
Nadiia Krzesinska <nb1985@ukr.net>
2025-10-09 15:49:18 +0000

    chore(img-pipe): allow undefined and use imgUrl pipe in Sidebar

--- tik-talk/src/app/helpers/pipes/img-pipe.ts
+++ tik-talk/src/app/helpers/pipes/img-pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImgPipe implements PipeTransform {
 
-  transform(value: string | null): string {
+  transform(value: string | null| undefined): string {
     if(!value) return '/assets/images/img1.png';
     return `https://icherniakov.ru/yt-course/${value}`;
   }
