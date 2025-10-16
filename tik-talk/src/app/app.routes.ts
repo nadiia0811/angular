5ea7b0579ce7bafa3f33d4748dca2c9ad27f255c
Nadya <nb1985@ukr.net>
2025-10-16 13:51:00 +0300

    chore(route): implement path for SettingsPage component

--- tik-talk/src/app/app.routes.ts
+++ tik-talk/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SearchPage } from './pages/search-page/search-page';
 import { ProfilePage } from './pages/profile-page/profile-page';
 import { Layout } from './common-ui/layout/layout';
 import { canActivateAuth } from './auth/auth.guard';
+import { SettingsPage } from './pages/settings-page/settings-page';
 
 export const routes: Routes = [
   {
     path: '', component: Layout, children: [
       { path: 'profile/:id', component: ProfilePage },
       { path: '', component: SearchPage },
+      { path: 'settings', component: SettingsPage}
     ],
     canActivate: [canActivateAuth]
   },
