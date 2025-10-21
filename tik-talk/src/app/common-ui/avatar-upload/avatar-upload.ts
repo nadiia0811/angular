bb280b199de5bd315633090a756c4986a90ab5b9
Nadya <nb1985@ukr.net>
2025-10-21 15:11:51 +0300

    feat(avatar-upload): add component for avatar image upload via drag-and-drop or file selection

--- tik-talk/src/app/common-ui/avatar-upload/avatar-upload.ts
+++ tik-talk/src/app/common-ui/avatar-upload/avatar-upload.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
-
+import { Dnd } from '../directives/dnd';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-avatar-upload',
-  imports: [],
+  imports: [Dnd, FormsModule],
   templateUrl: './avatar-upload.html',
   styleUrl: './avatar-upload.scss'
 })
 export class AvatarUpload {
   preview = signal<string>("/assets/images/user.png");
+  avatar = null;
 
   onKeyEnter(input: HTMLInputElement) {
     input.click();
@@ -16,6 +18,14 @@ export class AvatarUpload {
 
   onImageUpload(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0];
+    this.processFile(file);
+  }
+
+  onFileDropped(file: File) {
+    this.processFile(file);
+  }
+
+  processFile(file: File | null | undefined) {
     if(!file || !file.type.match("image")) { 
       return;
     }
