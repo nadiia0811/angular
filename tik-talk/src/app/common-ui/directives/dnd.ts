d0d7eced74797a6964c4b19549c0dd2069068f88
Nadya <nb1985@ukr.net>
2025-10-21 14:58:29 +0300

    feat(Dnd): add custom directive enabling file drag-and-drop support

---
+++ tik-talk/src/app/common-ui/directives/dnd.ts
@@ -0,0 +1,41 @@
+import { 
+  Directive,
+  HostBinding,
+  HostListener,
+  Output,
+  EventEmitter
+ } from '@angular/core';
+
+
+@Directive({
+  selector: '[appDnd]'
+})
+export class Dnd {
+  @Output() fileDropped = new EventEmitter<File>();  //custom event creation
+
+  @HostBinding('class.fileover')
+  fileover = false;
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileover = true;
+  }
+
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileover = false;
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileover = false;
+
+    this.fileDropped.emit(event.dataTransfer?.files[0]);
+  }
+}
